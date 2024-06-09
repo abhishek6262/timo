e12ce5909dd3f73e331b57fe8485788d7756a079
Abhishek Prakash <prakashabhishek6262@gmail.com>
2024-06-09 15:11:40 +0530

    fix windows permission issues

--- src/storage.rs
+++ src/storage.rs
@@ -1,59 +1,57 @@
 use std::{
-    fs::{self, File},
-    io::{BufReader, BufWriter, Seek, SeekFrom, Write},
+    fs::File,
+    io::{BufReader, BufWriter, Write},
     path::PathBuf,
 };
 
 use dirs::data_local_dir;
 
 pub struct Storage {
-    reader: File,
-    writer: File,
+    storage_path: PathBuf,
 }
 
 fn get_storage_path() -> PathBuf {
     let mut storage_path = data_local_dir().unwrap();
-
     storage_path.push("timodata.log");
     storage_path
 }
 
-fn ensure_path_exists(path: &PathBuf) {
-    if !path.exists() {
-        fs::write(path, "").unwrap();
-    }
-}
-
 impl Storage {
     pub fn new() -> Self {
-        let storage_path = get_storage_path();
-
-        ensure_path_exists(&storage_path);
-
-        let reader_path = storage_path.clone();
-        let writer_path = storage_path.clone();
-
-        let reader = File::options().read(true).open(reader_path).unwrap();
-        let writer = File::options().append(true).open(writer_path).unwrap();
-
-        Self { reader, writer }
+        Self {
+            storage_path: get_storage_path(),
+        }
     }
 
     pub fn clear(&self) {
-        let mut writer = BufWriter::new(&self.writer);
+        let file = File::options()
+            .create(true)
+            .write(true)
+            .open(&self.storage_path)
+            .unwrap();
 
-        self.writer.set_len(0).unwrap();
-        writer.seek(SeekFrom::Start(0)).unwrap();
+        file.set_len(0).unwrap();
     }
 
     pub fn write(&self, text: &str) {
         let text = text.to_owned() + "\n";
-        let mut writer = BufWriter::new(&self.writer);
+        let file = File::options()
+            .create(true)
+            .append(true)
+            .open(&self.storage_path)
+            .unwrap();
+        let mut writer = BufWriter::new(file);
 
-        writer.write(&text.as_bytes()).unwrap();
+        writer.write_all(text.as_bytes()).unwrap();
     }
 
-    pub fn read(&self) -> BufReader<&File> {
-        BufReader::new(&self.reader)
+    pub fn read(&self) -> BufReader<File> {
+        let file = File::options()
+            .create(true)
+            .read(true)
+            .open(&self.storage_path)
+            .unwrap();
+
+        BufReader::new(file)
     }
 }
